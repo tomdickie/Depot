2dd05cf2a979a84c46fe1b332a254ac3136e8222
Tom <tom@tom-laptop.(none)>
2010-02-01 18:32:31 +0000

    added untracked files to depot

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_depot_session',
+  :secret      => '170b670f5032438901c4c168547069717609a171fa1620cd1ad30a821473dbb3fa24711b4584604d3c83a8b20c70b5533f8c379629fa8001553ef3428ee72f61'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
